8a20dddae7935700967e682e29a81de3ab1645d3
Jake Teton-Landis <jake.tl@airbnb.com>
2015-02-21 19:24:37 -0800

    work with npm install

--- launch-cli.js
+++ launch-cli.js
@@ -1,3 +1,6 @@
 #!/usr/bin/env node
-require('babel/register') // switch language to Ecmascript 6
+require('babel/register')({
+  // switch language to Ecmascript 6
+  ignore: false
+})
 require('./cli.js') // actual entrypoint
--- launch-server.js
+++ launch-server.js
@@ -1,3 +1,6 @@
 #!/usr/bin/env node
-require('babel/register') // switch language to Ecmascript 6
+require('babel/register')({
+  // switch language to Ecmascript 6
+  ignore: false
+})
 require('./server.js') // actual entrypoint
